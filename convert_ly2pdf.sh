a9a656b221af5ac1a17c454c4238002b3c749a2b
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-18 14:19:23 +0100

    Add scripts to convert individual xml/ly files

---
+++ convert_ly2pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+PYTHON=$(which python2.7)
+LILYPOND=lilypond
+PREFIX="BN_lat_1112_Sequence"
+
+N=$1
+echo "Processing file #${N}"
+${LILYPOND} lilypond/${PREFIX}_${N}*.ly 2>&1 | tee logs_ly2pdf/output_ly2pdf_${N}.txt
+mv ${PREFIX}_${N}*.pdf pdf/
